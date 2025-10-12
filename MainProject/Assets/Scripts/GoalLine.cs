3ab4003c91dde801a012cf40734f9fad0dacc5d5
cdhakami-p <caseyhakami@gmail.com>
2025-10-12 17:08:24 -0400

    Added goal line detection collider & script

---
+++ MainProject/Assets/Scripts/GoalLine.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class GoalLine : MonoBehaviour
+{
+
+    public string ballTag = "Ball";
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(ballTag))
+        {
+            Debug.Log("Goal!");
+            //Later Code
+        }
+    }
+}
