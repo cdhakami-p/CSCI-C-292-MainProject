146f40774519f71cc7413afadc14f81518fcfd7c
cdhakami-p <caseyhakami@gmail.com>
2025-10-28 18:24:42 -0400

    Reset player spawning to random

--- MainProject/Assets/Scripts/PlayerSpawner.cs
+++ MainProject/Assets/Scripts/PlayerSpawner.cs
@@ -42,9 +42,8 @@ public class PlayerSpawner : MonoBehaviour
         Vector3 topSpawnPosition = topSpawn1.position;
         Vector3 bottomSpawnPosition = bottomSpawn1.position;
 
-        var topPlayer1 = Instantiate(playerPrefab[Random.Range(5, 5)], topSpawnPosition, Quaternion.Euler(0f,0f,180f));
-        var bottomPlayer1 = Instantiate(playerPrefab[Random.Range(1, 1)], bottomSpawnPosition, Quaternion.identity);
-        //0, playerPrefab.Length
+        var topPlayer1 = Instantiate(playerPrefab[Random.Range(0, playerPrefab.Length)], topSpawnPosition, Quaternion.Euler(0f,0f,180f));
+        var bottomPlayer1 = Instantiate(playerPrefab[Random.Range(0, playerPrefab.Length)], bottomSpawnPosition, Quaternion.identity);
 
         Outline(topPlayer1, Color.red);
         Outline(bottomPlayer1, Color.lightBlue);
